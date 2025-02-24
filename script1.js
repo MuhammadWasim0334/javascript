0712fed6f8cfeb7ad22a2b3cdd67783b0faee4ff
MuhammadWasim0334 <wasimafridi03348318830@gmail.com>
2025-02-24 10:51:15 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,59 @@
+
+console.log("Hello Java World!")
+
+// converting "10" to a number 
+let num = Number("10");
+console.log(num); // Output: 10
+
+
+// What will be the output of 5+"5"
+  console.log(5 + "5");  //In JavaScript, when you use the + operator with a number and a string, string concatenation occurs instead of arithmetic addition.
+
+
+
+// In JavaScript, the logical AND (&&) operator returns true only if both operands are true. Otherwise, it returns false.
+// // Example 1
+let isLoggedIn = true;
+let hasPermission = false;
+
+if (isLoggedIn && hasPermission)
+     {
+    console.log("Access granted!");
+} else {
+    console.log("Access denied!");
+}
+
+// Example 2
+let age = 20;       // Person's age (20 years old)
+let money = 5;      // Person has only $5
+
+if (age >= 18 && money >= 10) {
+    console.log("You can enter the park! 🎉"); 
+} else {
+    console.log("You cannot enter the park. ❌");
+}
+
+let a = 5, b = 10;
+
+console.log("Before Swap: a =", a, ", b =", b);
+
+// Swapping using a temporary variable
+let temp = a;
+a = b;
+b = temp;
+
+console.log("After Swap: a =", a, ", b =", b);
+
+console.log("Task1");
+let Name = "John";    // String (text)
+let ages = 25;         // Number
+let isStudent = true; // Boolean (true/false)
+
+console.log(Name, ages, isStudent);
+
+
+console.log("Task2");
+let numb = Number("100");
+console.log(numb); // Output: 10
+
+
